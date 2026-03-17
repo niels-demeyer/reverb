c9e9cc493cbcc2dc3e23386da69a2ad727ec187b
niels demeyer <contact.niels.demeyer@gmail.com>
2026-03-17 22:22:55 +0100

    hello world

---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"nelly"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
